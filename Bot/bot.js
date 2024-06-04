2c15022017856e3d3dfa04a0e533aa3b71898a3f
daffaaptar <daffaaptapratama@gmail.com>
2024-06-04 20:26:22 +0700

    fix layout

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,7 +2,16 @@ const { Telegraf } = require('telegraf')
 const TOKEN = '7170415970:AAHT91_gXLM_YDbe7Mpqg0-otpg2159PlvY'
 const bot = new Telegraf(TOKEN)
 
-bot.start((ctx) => ctx.reply('Welcome'))
+const web_link = "https://cheery-meringue-53791b.n etlify.app"
+
+bot.start((ctx) => {
+    ctx.reply('Welcome', {
+        reply_markup: {
+            inline_keyboard: [[{ text: "Play Botrexcoin", web_app: { url: web_link } }]],
+        },
+    });
+});
+
 
 bot.launch()
 
