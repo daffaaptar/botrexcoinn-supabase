471472779e3b85e77d3ef83eb24e5ba6e4cb5701
daffaaptar <daffaaptapratama@gmail.com>
2024-06-09 02:40:45 +0700

    fixing bugs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import Button from './component/button';
 import Dino from "./component/Dino/dino";
 import './App.css';
-import supabase from './supabaseClient';
+import supabase from './supabaseClient'; // Import koneksi ke Supabase
 
 const tele = window.Telegram.WebApp;
 
@@ -12,29 +12,29 @@ function App() {
 
   useEffect(() => {
     tele.ready();
+    fetchCoins(); // Panggil fungsi untuk memuat koin pengguna saat komponen dimuat
+    // Mendapatkan ID Telegram pengguna dari URL saat aplikasi dimuat
     const urlParams = new URLSearchParams(window.location.search);
     const telegramIdFromUrl = urlParams.get('telegram_id');
     setTelegramId(telegramIdFromUrl);
-
-    if (telegramIdFromUrl) {
-      fetchCoins(telegramIdFromUrl);
-    }
   }, []);
 
-  const fetchCoins = async (telegramId) => {
+  const fetchCoins = async () => {
     try {
-      const { data, error } = await supabase
-        .from('users')
-        .select('coins')
-        .eq('telegram_id', telegramId);
+      if (telegramId) {
+        const { data, error } = await supabase
+          .from('users')
+          .select('coins')
+          .eq('telegram_id', telegramId);
 
-      if (error) {
-        console.error('Error fetching coins:', error.message);
-        return;
-      }
+        if (error) {
+          console.error('Error fetching coins:', error.message);
+          return;
+        }
 
-      if (data && data.length > 0) {
-        setCoins(data[0].coins);
+        if (data && data.length > 0) {
+          setCoins(data[0].coins); // Mengatur jumlah koin dari data yang diperoleh dari Supabase
+        }
       }
     } catch (error) {
       console.error('Error fetching coins:', error.message);
@@ -54,18 +54,16 @@ function App() {
   };
 
   const handleGameOver = async (score) => {
-    const newCoins = coins + score;
-    setCoins(newCoins);
-
+    setCoins(prevCoins => prevCoins + score);
     try {
       if (telegramId) {
-        const { error } = await supabase
+        // Simpan jumlah koin pengguna ke Supabase saat permainan berakhir
+        const { data,error } = await supabase
           .from('users')
           .upsert([{ 
             telegram_id: telegramId, 
-            coins: newCoins 
+            coins: coins + score 
           }], { 
-            onConflict: ['telegram_id'],
             returning: 'minimal' 
           });
 
@@ -83,8 +81,10 @@ function App() {
       {/* Coin Container */}
       <div className="flex flex-col items-center justify-center mt-10">
         <div className="bg-slate-700 rounded-md p-4 mb-2 flex items-center justify-center">
+          {/* Logo Coin */}
           <img src="./btx.png" alt="Coin Logo" className="w-20 h-20" />
           <div className="bg-slate-500 rounded-md px-2 m-5 text-white text-2xl font-bold">
+            {/* Jumlah Coin */}
             {coins.toLocaleString()}
           </div>
         </div>
@@ -94,6 +94,7 @@ function App() {
         <Dino onGameOver={handleGameOver} />
       </div>
 
+      {/* Button Container */}
       <div className="flex bg-slate-700 rounded-md my-5 px-5 justify-center">
         <div className="flex justify-center items-center mb-5 mt-5">
           <Button type="earn" onClick={handleEarnClick} />
